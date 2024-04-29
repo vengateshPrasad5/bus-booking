4dd4206d32ce435253cd6ef57b92adc63f7243ae
Dasrap <vengatesh@automaton.com>
2024-04-29 19:08:44 +0530

    navbar added

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+
+function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleDropdown = () => {
+    setIsOpen(prevState => !prevState);
+  };
+  return (
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">
+          Bus Booking App
+        </a>
+        <div className="collapse navbar-collapse">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0"></ul>
+          <a className="nav-link">
+               Sign In
+          </a>
+          <li className="navbar-nav nav-item dropdown">
+            <a
+              className="nav-link dropdown-toggle"
+              role="button"
+              onClick={toggleDropdown}
+              aria-expanded={isOpen ? "true" : "false"}
+            >
+              Welcome Username
+            </a>
+            <ul className={`dropdown-menu ${isOpen ? 'show' : ''}`}>
+              <li>
+                <a className="dropdown-item" href="#">
+                  UserProfile
+                </a>
+              </li>
+              <li>
+                <a className="dropdown-item" href="#">
+                  Booking History
+                </a>
+              </li>
+              <li>
+                <hr className="dropdown-divider"></hr>
+              </li>
+              <li>
+                <a className="dropdown-item" href="#">
+                  Logout
+                </a>
+              </li>
+            </ul>
+          </li>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
