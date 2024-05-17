e18e0c06fac46fe6c278b6337e22cbf7be2358ad
Dasrap <vengatesh@automaton.com>
2024-05-17 23:27:17 +0530

    bus list mapping added

--- src/components/BusList.jsx
+++ src/components/BusList.jsx
@@ -1,11 +1,63 @@
 import React from "react";
+import { Button } from "react-bootstrap";
 
 function BusList({ filteredBus }) {
+  const calulateTimeDiff = (arrTime, depTime) => {
+    const parseTime = (time) => {
+      const [timePart, period] = time.split(" ");
+      let [hours, minutes] = timePart.split(":");
+      hours = parseInt(hours);
+      minutes = parseInt(minutes);
+      if (period === "PM" && hours !== 12) hours += 12;
+      return new Date(2024, 0, 1, hours, minutes);
+    };
+
+    const departure = parseTime(depTime);
+    const arrival = parseTime(arrTime);
+    console.log(departure);
+    console.log(arrival);
+    let diff = arrival - departure;
+    if (diff < 0) diff += 24 * 60 * 60 * 1000;
+    console.log(diff);
+    const hours = Math.floor(diff / (1000 * 60 * 60));
+    const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+
+    return `${hours} hours and ${minutes} minutes`;
+  };
   return (
     <>
-      <div className="container row border-rounded shadow my-3 mx-auto p-3">
-        <p className="text-center fw-bold">Buses List</p>
-      </div>
+      {filteredBus.map((data, index) => (
+        <div
+          key={index}
+          className="container row border-rounded shadow my-3 mx-auto p-3"
+        >
+          <h3 className="mb-3">{data.name}</h3>
+          <p className="fw-normal">{data.busType}</p>
+          <div className="d-flex justify-content-evenly align-items-center">
+            <div>
+              <p className="fw-bold m-0"> {data.source}</p>
+              <p className="fw-bold">{data.departureTime}</p>
+            </div>
+            <div>
+              ----------
+              {calulateTimeDiff(data.arrivalTime, data.departureTime)}
+              ----------
+            </div>
+            <div>
+              <p className="fw-bold m-0"> {data.destination}</p>
+              <p className="fw-bold"> {data.arrivalTime}</p>
+            </div>
+            <div>
+              <p className="text-center">
+                Price: <span className="fw-bold">{data.price}</span>
+              </p>
+              <Button variant="primary" style={{ width: 150 }}>
+                View Seats
+              </Button>
+            </div>
+          </div>
+        </div>
+      ))}
     </>
   );
 }
