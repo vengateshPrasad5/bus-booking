065058f7ad5084bd476acf6a446b37724fcd2f0c
VengateshPrasad <venkuvenky2@gmail.com>
2024-05-29 19:09:39 +0530

    added auth components

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,37 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
 import "bootstrap/dist/css/bootstrap.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import SearchBus from "./components/SearchBus";
 import { useState } from "react";
 import { locations } from "./utils";
 import BusLayout from "./components/BusLayout";
 import BookingForm from "./components/BookingForm";
-
+import { isUserLoggedIn } from "./service/authService";
+import LoginComponent from "./components/LoginComponent";
+import RegisterComponent from "./components/RegisterComponent";
 function App() {
   const [search, setSearch] = useState({
     from: locations[0],
     to: locations[1],
     date: "",
   });
-
   const [selectedSeats, setSelectedSeats] = useState([]);
+
+  function AuthenticatedRoute({ children }) {
+    const isAuth = isUserLoggedIn();
+
+    if (isAuth) {
+      return children;
+    }
+
+    return <Navigate to="/" />;
+  }
+
   return (
     <div>
-      <Navbar />
       <BrowserRouter>
+        <Navbar />
         <Routes>
           <Route
             path="/"
@@ -28,6 +40,8 @@ function App() {
           <Route
             path="/bus/:id"
             element={
+              // <AuthenticatedRoute>
+              //   </AuthenticatedRoute>
               <BusLayout
                 selectedSeats={selectedSeats}
                 setSelectedSeats={setSelectedSeats}
@@ -37,9 +51,13 @@ function App() {
           <Route
             path="/bus/addPassenger"
             element={
-              <BookingForm selectedSeats={selectedSeats} search={search} />
+              <AuthenticatedRoute>
+                <BookingForm selectedSeats={selectedSeats} search={search} />
+              </AuthenticatedRoute>
             }
           />
+          <Route path="/login" element={<LoginComponent />} />
+          <Route path="/register" element={<RegisterComponent />} />
         </Routes>
       </BrowserRouter>
     </div>
