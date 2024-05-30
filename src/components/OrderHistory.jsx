255172f877dc6b9c4134cf56637f032dafc4d24d
VengateshPrasad <venkuvenky2@gmail.com>
2024-05-30 21:50:04 +0530

    null display message

--- src/components/OrderHistory.jsx
+++ src/components/OrderHistory.jsx
@@ -29,44 +29,51 @@ function OrderHistory() {
         <Loader />
       ) : (
         <div>
-          <table className="table table-bordered table-striped">
-            <thead>
-              <tr>
-                <th>Id</th>
-                <th>Name</th>
-                <th>Age</th>
-                <th>Gender</th>
-                <th>Seat No</th>
-                <th>Bus Name</th>
-                <th>Bus Type</th>
-                <th>Source</th>
-                <th>Destination</th>
-                <th>Departure Time</th>
-                <th>Arrival Time</th>
-              </tr>
-            </thead>
-            <tbody>
-              {history.map((item, index) => (
-                <tr key={index}>
-                  <td>{index + 1}</td>
-                  <td>{item.name}</td>
-                  <td>{item.age}</td>
-                  <td>{item.gender}</td>
-                  <td>{item.seat.seatNo}</td>
-                  <td>{item.bus.name}</td>
-                  <td>{item.bus.busType}</td>
-                  <td>{item.bus.source}</td>
-                  <td>{item.bus.destination}</td>
-                  <td>
-                    {item.bus.departureDate} {item.bus.departureTime}
-                  </td>
-                  <td>
-                    {item.bus.arrivalDate} {item.bus.arrivalTime}
-                  </td>
+          {history && history.length > 0 ? (
+            <table className="table table-bordered table-striped">
+              <thead>
+                <tr>
+                  <th>Id</th>
+                  <th>Name</th>
+                  <th>Age</th>
+                  <th>Gender</th>
+                  <th>Seat No</th>
+                  <th>Bus Name</th>
+                  <th>Bus Type</th>
+                  <th>Source</th>
+                  <th>Destination</th>
+                  <th>Departure Time</th>
+                  <th>Arrival Time</th>
                 </tr>
-              ))}
-            </tbody>
-          </table>
+              </thead>
+
+              <tbody>
+                {history?.map((item, index) => (
+                  <tr key={index}>
+                    <td>{index + 1}</td>
+                    <td>{item.name}</td>
+                    <td>{item.age}</td>
+                    <td>{item.gender}</td>
+                    <td>{item.seat.seatNo}</td>
+                    <td>{item.bus.name}</td>
+                    <td>{item.bus.busType}</td>
+                    <td>{item.bus.source}</td>
+                    <td>{item.bus.destination}</td>
+                    <td>
+                      {item.bus.departureDate} {item.bus.departureTime}
+                    </td>
+                    <td>
+                      {item.bus.arrivalDate} {item.bus.arrivalTime}
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          ) : (
+            <h4 className="text-center my-4">
+              No Booking Done, Please Visit after booking
+            </h4>
+          )}
         </div>
       )}
     </div>
