6a2fef34651baa58f3ab1b93d700cd6ecbc320d5
VengateshPrasad <venkuvenky2@gmail.com>
2024-06-01 18:35:48 +0530

    loader and handle auth route

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,16 @@
-import React, { useContext, useState } from "react";
+import React, { useState } from "react";
+import {
+  isUserLoggedIn,
+  getLoggedInUser,
+  logout,
+} from "../service/authService";
+import { useNavigate } from "react-router-dom";
 
 function Navbar() {
-  // const{isUserLoggedIn} = useContext(AuthContext)
   const [isOpen, setIsOpen] = useState(false);
+  const isAuth = isUserLoggedIn();
+  const userName = getLoggedInUser();
+  const navigate = useNavigate();
 
   const toggleDropdown = () => {
     setIsOpen((prevState) => !prevState);
@@ -10,41 +18,65 @@ function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg bg-body-tertiary">
       <div className="container-fluid">
-        <a className="navbar-brand" href="#">
+        <a
+          className="navbar-brand"
+          href=""
+          onClick={() => {
+            navigate("/");
+          }}
+        >
           Bus Booking App
         </a>
         <ul className="navbar-nav me-auto mb-2 mb-lg-0"></ul>
-        <a className="nav-link">Sign In</a>
-        <li className="navbar-nav nav-item dropdown">
-          <a
-            className="nav-link dropdown-toggle"
-            role="button"
-            onClick={toggleDropdown}
-            aria-expanded={isOpen ? "true" : "false"}
-          >
-            Welcome Username
+        {isAuth ? (
+          <li className="navbar-nav nav-item dropdown">
+            <a
+              className="nav-link dropdown-toggle"
+              role="button"
+              onClick={() => toggleDropdown()}
+              aria-expanded={isOpen ? "true" : "false"}
+            >
+              Welcome {userName}
+            </a>
+            <ul className={`dropdown-menu ${isOpen ? "show" : ""}`}>
+              <li>
+                <a
+                  className="dropdown-item"
+                  onClick={() => navigate("/userProfile")}
+                >
+                  UserProfile
+                </a>
+              </li>
+              <li>
+                <a
+                  className="dropdown-item"
+                  onClick={() => navigate("/bus/bookingHistory")}
+                >
+                  Booking History
+                </a>
+              </li>
+              <li>
+                <hr className="dropdown-divider"></hr>
+              </li>
+              <li>
+                <a
+                  className="dropdown-item"
+                  onClick={() => {
+                    logout();
+                    navigate("/");
+                    alert("Logged out Successfully");
+                  }}
+                >
+                  Logout
+                </a>
+              </li>
+            </ul>
+          </li>
+        ) : (
+          <a className="nav-link" onClick={() => navigate("/login")}>
+            Sign In
           </a>
-          <ul className={`dropdown-menu ${isOpen ? "show" : ""}`}>
-            <li>
-              <a className="dropdown-item" href="#">
-                UserProfile
-              </a>
-            </li>
-            <li>
-              <a className="dropdown-item" href="#">
-                Booking History
-              </a>
-            </li>
-            <li>
-              <hr className="dropdown-divider"></hr>
-            </li>
-            <li>
-              <a className="dropdown-item" href="#">
-                Logout
-              </a>
-            </li>
-          </ul>
-        </li>
+        )}
       </div>
     </nav>
   );
