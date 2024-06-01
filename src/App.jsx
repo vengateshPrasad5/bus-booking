53cec081c29d92a631766ef60788bf074b9dbaef
VengateshPrasad <venkuvenky2@gmail.com>
2024-06-01 18:32:07 +0530

    jwt logout handled

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
 import "bootstrap/dist/css/bootstrap.css";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SearchBus from "./components/SearchBus";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { locations } from "./utils";
 import BusLayout from "./components/BusLayout";
 import BookingForm from "./components/BookingForm";
@@ -15,6 +15,7 @@ import OrderHistory from "./components/OrderHistory";
 import UserProfile from "./components/UserProfile";
 import PrivateOutlet from "./components/PrivateOutlet";
 import Payment from "./components/Payment";
+import { getToken, isTokenExpired } from "./service/authService";
 function App() {
   const [search, setSearch] = useState({
     from: locations[0],
@@ -23,6 +24,7 @@ function App() {
   });
   const [selectedBus, setSelectedBus] = useState();
   const [selectedSeats, setSelectedSeats] = useState([]);
+  console.log("isTokenExpired", isTokenExpired(getToken()));
 
   return (
     <BrowserRouter>
@@ -41,12 +43,14 @@ function App() {
             }
           />
           <Route
-            path="/bus/addPassenger"
+            path="/checkout"
             element={
               <BookingForm
                 selectedSeats={selectedSeats}
                 setSelectedSeats={setSelectedSeats}
                 search={search}
+                setSearch={setSearch}
+                setSelectedBus={setSelectedBus}
                 selectedBus={selectedBus}
               />
             }
